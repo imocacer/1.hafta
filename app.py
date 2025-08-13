537b3eb4d0934afa5bd857c7b221084152c7a156
İmran Alpsoy <212855111@mezun.usak.edu.tr>
2025-08-13 22:56:51 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# user1 = "56465465132132"
+# user2 = "56654654654656"
+
+# door = 2
+
+# print(user1,door,"kere giriş yaptı.")
+
+# dogumYili = int(input("doğum yılınızı giriniz:"))
+# mYil = 2025
+# print(f"yaşınız: {mYil - dogumYili}")
+#tersten yazdırma
+
+isim = input("Adınızı giriniz: ")
+terstenYazdirma = isim[::-1]
+print(f"İsminizin tersten yazılışı: {terstenYazdirma.capitalize()}")
